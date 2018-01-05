6ad056b2f4df73cee1b041d46c89716370034404
Changsheng Liu <lcssxl@gmail.com>
2018-01-05 13:02:08 +0800

    优化调试
    完善调整时间、坐标等参数以及流程修改

--- kog.py
+++ kog.py
@@ -6,14 +6,12 @@ import os
 from time import sleep
 
 # 屏幕分辨率
-device_x, device_y = 1920, 1080
-
-# 通关模式：1=重新挑战 -> 挑战界面，2=重新挑战-> 更换阵容
-game_mode = 2
-
+device_x = 1920
+device_y = 1080
+# 通关顺序：0=再次挑战 -> 1=闭关 -> 2=跳过 -> 3=跳过 -> 4=点击屏幕继续 ->->
+game_mode = 1
 # 各步骤等待间隔
-step_wait = [3, 13, 24, 3, 3]
-
+step_wait = [5, 48, 6, 5, 10]
 # 刷金币次数
 repeat_times = 60
 
@@ -25,31 +23,36 @@ logging.basicConfig(format='%(asctime)s %(message)s',
 
 def tap_screen(x, y):
     """calculate real x, y according to device resolution."""
-    base_x, base_y = 1920, 1080
+    base_x = 1920.0
+    base_y = 1080.0
     real_x = int(x / base_x * device_x)
     real_y = int(y / base_y * device_y)
     os.system('adb shell input tap {} {}'.format(real_x, real_y))
 
 
 def do_money_work():
-    if game_mode == 1:
-        logging.debug('#0 start the game')
-        tap_screen(1600, 970)
-        sleep(step_wait[0])
 
-    logging.debug('#1 ready, go!!!')
-    tap_screen(1450, 910)
+    logging.debug('#0 - start the game again')
+    tap_screen(1600, 1000)
+    sleep(step_wait[0])
+
+    logging.debug('#1 - ready, go!!!')
+    tap_screen(1500, 920)
     sleep(step_wait[1])
 
-    logging.debug('#2 auto power on!')
-    tap_screen(1780, 40)
+    #for i in range(2):
+    #    logging.debug('#' + str(2+i) + ' - auto power on!')
+    #    tap_screen(1800, 80)
+    #    sleep(2+i)
+    logging.debug('#2 - auto power on!')
+    tap_screen(1800, 80)
+    sleep(step_wait[2])
+    logging.debug('#3 - auto power on!')
+    tap_screen(1800, 80)
+    sleep(step_wait[3])
 
-    for i in range(step_wait[2]):
-        tap_screen(1720, 80)
-        sleep(1)
-
-    logging.debug('#3 do it again...\n')
-    tap_screen(1600, 980)
+    logging.debug('#4 - do it again...\n')
+    tap_screen(1000, 1024)
     sleep(step_wait[4])
 
 
